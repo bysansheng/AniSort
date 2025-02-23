e3c60fcb1b0953e896a9370f85cd97eefc72ef2b
by三生 <105970176+bysansheng@users.noreply.github.com>
2025-02-23 17:47:13 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ PATTERN: list = [
     {
         "type": "IV"，
         "regex": r"(?i)(IV|Interview)[ _-]?(\d*)"，
-        "normalize": "Interviews/{ani_name} - IV{number:02d}.{file_type}"，
+        "normalize": "Interviews/{ani_name} - IV{number:02d}"，
         "priority": 4
     }，
     {
         "type": "SP"，
         "regex": r"(?i)(SP|OVA|EXTRAS|Special|特別編|特别篇|S00E)[ _-]?(\d*)"，
-        "normalize": "Other/{ani_name} - SP{number:02d}.{file_type}"，
+        "normalize": "Other/{ani_name} - SP{number:02d}"，
         "priority": 1
     }，
 
@@ -22,25 +22,25 @@ PATTERN: list = [
     {
         "type": "SE_EP"，
         "regex": r"(?i)(?:S|Season)[ _-]?(\d+)[ _-]?(?:E|Episode)[ _-]?(\d+)"，
-        "normalize": "Season {season:02d}/{ani_name} - S{season:02d}E{number:02d}.{file_type}"，
+        "normalize": "Season {season:02d}/{ani_name} - S{season:02d}E{number:02d}"，
         "priority": 2
     }，
     {
         "type": "EP"，
         "regex": r"(?i)(?:EP|E|Episode|第)[ _-]?(\d+)"，
-        "normalize": "Season {season:02d}/{ani_name} - S{season:02d}E{number:02d}.{file_type}"，
+        "normalize": "Season {season:02d}/{ani_name} - S{season:02d}E{number:02d}"，
         "priority": 5
     }，
     {
         "type": "EP"，
         "regex": r"(?i)\b(\d{2,3})(?:v\d+)?\.(?:mkv|mp4)"，
-        "normalize": "Season {season:02d}/{ani_name} - S{season:02d}E{number:02d}.{file_type}"，
+        "normalize": "Season {season:02d}/{ani_name} - S{season:02d}E{number:02d}"，
         "priority": 3
     }，
     {
         "type": "EP"，
         "regex": r"(?i)\[(\d{2,3})\].*\.(?:mkv|mp4)"，
-        "normalize": "Season {season:02d}/{ani_name} - S{season:02d}E{number:02d}.{file_type}"，
+        "normalize": "Season {season:02d}/{ani_name} - S{season:02d}E{number:02d}"，
         "priority": 3
     }，
 
@@ -48,7 +48,7 @@ PATTERN: list = [
     {
         "type": "CD"，
         "regex": r"(?i)(CD|BD|DVD|DISC|Disk|Vol)[ _-]?(\d*)"，
-        "normalize": "Other/{ani_name} - CD{number:02d}.{file_type}"，
+        "normalize": "Other/{ani_name} - CD{number:02d}"，
         "priority": 4
     }，
 
@@ -56,7 +56,7 @@ PATTERN: list = [
     {
         "type": "OP/ED"，
         "regex": r"(?i)(OP|ED|NCED\d*_EP\d+|NCOP|NCED)[ _-]?(\d*)"，
-        "normalize": "Other/{ani_name} - {raw_match}.{file_type}"，
+        "normalize": "Other/{ani_name} - {raw_match}"，
         "priority": 4
     }，
 
@@ -64,7 +64,7 @@ PATTERN: list = [
     {
         "type": "NC"，
         "regex": r"(?i)(NC.*EP)(\d*)"，
-        "normalize": "Other/{ani_name} - {raw_match}.{file_type}"，
+        "normalize": "Other/{ani_name} - {raw_match}"，
         "priority": 4
     }，
 
@@ -72,7 +72,7 @@ PATTERN: list = [
     {
         "type": "Preview"，
         "regex": r"(?i)(Preview|Prev)[ _-]?(\d*)"，
-        "normalize": "Trailers/{ani_name} - Preview{number:02d}.{file_type}"，
+        "normalize": "Trailers/{ani_name} - Preview{number:02d}"，
         "priority": 4
     }，
 
@@ -88,7 +88,7 @@ PATTERN: list = [
     {
         "type": "PV"，
         "regex": r"(?i)(PV|Trailer|Teaser)[ _-]?(\d*)"，
-        "normalize": "Trailers/{ani_name} - {raw_match}.{file_type}"，
+        "normalize": "Trailers/{ani_name} - {raw_match}"，
         "priority": 4
     }，
 
@@ -96,25 +96,25 @@ PATTERN: list = [
     {
         "type": "CM"，
         "regex": r"(?i)(CM|SPOT)[ _-]?(\d*)"，
-        "normalize": "Trailers/{ani_name} - CM{raw_match}.{file_type}"，
+        "normalize": "Trailers/{ani_name} - CM{raw_match}"，
         "priority": 4
     }，
 
     # 菜单
     {
         "type": "Menu"，
-        "regex": r"(?i)(Menu)[ _-]?(\d+_\d+|\d+)",
-        "normalize": "Other/{ani_name} - Menu{number:02d}.{file_type}",
+        "regex": r"(?i)(Menu)[ _-]?(\d+_\d+|\d+)"，
+        "normalize": "Other/{ani_name} - Menu{number:02d}"，
         "priority": 4
-    },
+    }，
 
     # 音乐视频
     {
-        "type": "MV",
-        "regex": r"(?i)\b(MV)[ _-]?(\d*)",
-        "normalize": "Other/{ani_name} - MV{number:02d}.{file_type}",
+        "type": "MV"，
+        "regex": r"(?i)\b(MV)[ _-]?(\d*)"，
+        "normalize": "Other/{ani_name} - MV{number:02d}"，
         "priority": 4
-    },
+    }，
 ]
 
 PATTERN.sort(key=lambda x: x['priority'])
